565b7e9523682150cb90d1311ab515a6383286bb
21cloudocean <437508630@qq.com>
2022-08-05 21:53:29 +0800

    es-header-basic

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>App根组件</h1>
+    <!-- 使用 es-header 组件 -->
+    <EsHeader></EsHeader>
+  </div>
+</template>
+
+<script>
+// 导入 header 组件
+import EsHeader from './components/es-header/EsHeader.vue'
+export default {
+  name: 'MyApp',
+  components: {
+    // 注册 header 组件
+    EsHeader
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/components/es-header/EsHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>EsHeader 组件</div>
+</template>
+
+<script>
+export default {
+  name: 'EsHeader'
+}
+</script>
+
+<style lang="less" scoped></style>
